fc21e26c908aafd5cd355f4222983d92f0b517da
John Gallagher <john@synapticmishap.co.uk>
2023-10-09 17:31:44 +0100

    doc: set next steps

--- refactoring/1_clarielle/test/app_test.rb
+++ refactoring/1_clarielle/test/app_test.rb
@@ -1,6 +1,10 @@
 require "minitest/autorun"
 require "minitest/pride"
 
+# 1. What is the goal?
+# Change the add an album so that it uses the Spotify API
+#
+# 2. Get the code working!
 class AppTest < Minitest::Test
   def test_something
     assert_equal 1, 2
