6a24dd3eb5e333e951e9b1d12a6bec13e1ef66a5
John Gallagher <john@synapticmishap.co.uk>
2023-10-10 22:34:40 +0100

    test: add menu

--- test/app_test.rb
+++ test/app_test.rb
@@ -2,28 +2,40 @@ require_relative 'test_helper'
 require_relative '../app'
 
 describe 'App' do
-  it 'when I launch the app it will show me options' do
-    input = StringIO.new("8\nPop\ny\n2020-01-01\ny\nRock")
-    output = StringIO.new
+  it 'when I start up the app it shows me a menu' do
+    actual_menu = app_home_screen
+
+    expected_menu = [
+      'Please choose an option according to the numbers on the dashboard:',
+      '    1# List all books',
+      '    2# List all music albums',
+      '    3# List of games',
+      "    4# List all genres (e.g 'Comedy', 'Thriller')",
+      "    5# List all labels (e.g. 'Gift', 'New')",
+      "    6# List all authors (e.g. 'Stephen King')",
+      '    7# Add a book',
+      '    8# Add a music album',
+      '    9# Add a game',
+      '    10# Exit'
+    ].join("\n")
+    assert_equal expected_menu, actual_menu
+  end
+
+  it 'when I add a music album it asks me questions about the album' do
     music_album_store = StringIO.new
     genres_store = StringIO.new
-    app = App.new(input:, output:, music_album_store:, genres_store:)
-    app.start
-    menu_and_output = %(Please choose an option according to the numbers on the dashboard:
-    1# List all books
-    2# List all music albums
-    3# List of games
-    4# List all genres (e.g 'Comedy', 'Thriller')
-    5# List all labels (e.g. 'Gift', 'New')
-    6# List all authors (e.g. 'Stephen King')
-    7# Add a book
-    8# Add a music album
-    9# Add a game
-    10# Exit
-_____ADD A MUSIC ALBUM_____
 
-What is the album name? Is it on Spotify? (y/n) What is the date of publication? (YYYY-MM-DD-) Is it archived? (y/n) What is the genre of the music album? A music album is created successfully)
-    assert_equal output.string.strip, menu_and_output.strip
+    actual_questions = extract_questions_from(add_music_album(music_album_store:, genres_store:))
+
+    expected_questions = [
+      'What is the album name?',
+      'Is it on Spotify? (y/n)',
+      'What is the date of publication? (YYYY-MM-DD-)',
+      'Is it archived? (y/n)',
+      'What is the genre of the music album?',
+      'A music album is created successfully'
+    ].join(' ')
+    assert_equal expected_questions, actual_questions
   end
 
   it 'when I add a music album then that same album will show when I list all music albums' do
@@ -40,21 +52,31 @@ What is the album name? Is it on Spotify? (y/n) What is the date of publication?
   it 'when I add a music album in the genre of Rock and then list out all genres Rock should be in that list' do
     music_album_store = StringIO.new
     genres_store = StringIO.new
-    add_music_album(genre: 'Rock', music_album_store:, genres_store:)
+    add_music_album(genre: 'Blues', music_album_store:, genres_store:)
 
     genres = list_genres(music_album_store:, genres_store:)
 
-    assert_equal ["0 - Rock"], genres
+    assert_equal ['0 - Blues'], genres
   end
-  
+
   private
 
-  def add_music_album(name: 'Pop', publish_date: '2020-01-01', genre: 'Rock', music_album_store:, genres_store:)
-    input = StringIO.new("8\nPop\ny\n2020-01-01\ny\nRock")
+  def add_music_album(music_album_store:, genres_store:, name: 'Pop', publish_date: '2020-01-01', genre: 'Rock')
+    input = StringIO.new("8\n#{name}\ny\n#{publish_date}\ny\n#{genre}")
     output = StringIO.new
     app = App.new(input:, output:, music_album_store:, genres_store:)
     app.start
-    app
+    output.string
+  end
+
+  def app_home_screen
+    music_album_store = StringIO.new
+    genres_store = StringIO.new
+    input = StringIO.new('2')
+    output = StringIO.new
+    app = App.new(input:, output:, music_album_store:, genres_store:)
+    app.start
+    output.string.split('_____').first.strip
   end
 
   def list_music_albums(music_album_store:, genres_store:)
@@ -75,5 +97,11 @@ What is the album name? Is it on Spotify? (y/n) What is the date of publication?
     heading = '_____LIST OF GENRE_____'
     _menu, list_of_genres = output.string.split(heading)
     list_of_genres.strip.split("\n")
+  end
+
+  def extract_questions_from(output)
+    heading = '_____ADD A MUSIC ALBUM_____'
+    _menu, questions = output.split(heading)
+    questions.strip
   end
 end
